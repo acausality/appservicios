856ce2f27d335f7aba499e50022698102d46a75d
Causality <alitycaus@gmail.com>
2018-04-18 15:16:40 -0300

    Error en nombre de DB de application.properties Agregado el script para crear la DB

---
+++ ScriptsSQL/crear-db.sql
@@ -0,0 +1,26 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema app_servicios
+-- -----------------------------------------------------
+CREATE DATABASE app_servicios;
+USE app_servicios;
+DROP USER usuario_app_servicios; /* por mas que no exista, es necesaria
+esta linea y la que sigue debido a un bug de MySQL */
+FLUSH PRIVILEGES;
+CREATE USER usuario_app_servicios IDENTIFIED BY 'app6529@Serv129457';
+
+GRANT ALL ON `app_servicios`.* TO 'usuario_app_servicios';
+GRANT SELECT ON TABLE `app_servicios`.* TO 'usuario_app_servicios';
+GRANT SELECT, INSERT, TRIGGER ON TABLE `app_servicios`.* TO 'usuario_app_servicios';
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE `app_servicios`.* TO 'usuario_app_servicios';
+/*GRANT EXECUTE ON ROUTINE 'app_servicios'.* TO 'usuario_app_servicios';
+linea comentada porque tiraba un error, por mas que fue generada por el workbench*/
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
