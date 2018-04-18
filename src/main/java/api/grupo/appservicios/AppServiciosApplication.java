864921a1402cb86b6175ca7f4fbcc7cba4c51fc5
Causality <alitycaus@gmail.com>
2018-04-18 14:27:34 -0300

    Primer commit

---
+++ src/main/java/api/grupo/appservicios/AppServiciosApplication.java
@@ -0,0 +1,12 @@
+package api.grupo.appservicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppServiciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppServiciosApplication.class, args);
+	}
+}
---
+++ src/test/java/api/grupo/appservicios/AppServiciosApplicationTests.java
@@ -0,0 +1,21 @@
+package api.grupo.appservicios;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppServiciosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	//testear conexion a base de datos
+	public void testDb() {
+		
+	}
+}
